89fe08c87b0fab8978e7bbe2950474affed3d3e8
devast8a <devast8a@whitefirex.com>
2016-03-03 16:59:25 +1030

    Add initialization and add commands

--- src/combinedrepo/__init__.py
+++ src/combinedrepo/__init__.py
@@ -41,17 +41,26 @@ def load_config(path):
 
         return (config, extra)
 
-def path_to_subtree_config((config, extra), path):
+def calculate_subrepository_relative_path((config, extra), path):
+    """
+        Calculate the relative path to a subrepository from the
+        config file.
+    """
+    path = path.replace(extra["config.dir"], '')
+    path = path[1:] # Strip leading slash
+    path = path.replace('\\', '/')
+
+    return path
+
+def path_to_subtree_config(config, path):
     """
         Return the subtree config for the given path. If no config could
         be found, return None instead
     """
 
-    path = path.replace(extra["config.dir"], '')
-    path = path[1:] # Strip leading slash
-    path = path.replace('\\', '/')
-
-    return config["repositories"].get(path)
+    return config[0]["repositories"].get(
+        calculate_subrepository_relative_path(config, path)
+    )
 
 def call((config, extra), args):
     """
@@ -59,9 +68,9 @@ def call((config, extra), args):
     """
     subprocess.Popen(args, cwd=extra["repository"]).wait()
 
-def merge(config, path):
+def push(config, path):
     """
-        Merge the given repository path into the combined repository.
+        Push changes in the given repository to the combined repository.
     """
 
     stConfig = path_to_subtree_config(config, path)
@@ -83,28 +92,100 @@ def cmd_svn_dcommit(args):
     merge(config, repo)
     call(config, ["git", "svn", "dcommit"])
 
-def cmd_merge(args):
+def cmd_push(args):
     """
-        Merge the repository with the combined repository
+        Push changes in the given repository to the combined repository.
     """
     repo = os.getcwd()
     config = load_config(find_config(repo, CONFIG_FILENAME))
-    merge(config, repo)
+    push(config, repo)
 
 def cmd_add(args):
-    raise NotImplementedError("Add command not yet implemented")
+    repo = os.getcwd()
+    config = load_config(find_config(repo, CONFIG_FILENAME))
+    path = calculate_subrepository_relative_path(config, repo)
+
+    (cfg, extra) = config
+
+    if path in cfg["repositories"]:
+        print "You have already added this path to the repository!"
+        return
+
+    cfg["repositories"][path] = {
+        "prefix": args.prefix,
+        "branch": args.branch,
+    }
+
+    with open(extra["config.path"], 'w') as f:
+        json.dump(cfg, f, indent=4)
+
+    gitargs = ["git", "subtree", "add"]
+    gitargs.append("--prefix="+args.prefix)
+    gitargs.append(repo)
+    gitargs.append(args.branch)
+
+    call(config, gitargs)
+
+def cmd_init(args):
+    config = {
+        "combined-repository": args.repository,
+        "repositories": {},
+    }
+
+    with open(CONFIG_FILENAME, 'w') as f:
+        json.dump(config, f, indent=4)
 
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
-    # merge
-    merge = subparsers.add_parser("merge")
-    merge.set_defaults(func=cmd_merge)
+    ##################################################
+    # add
+    add = subparsers.add_parser(
+        "add",
+        help="Add a new sub-repository to the combined repository"
+    )
+    add.set_defaults(func=cmd_add)
 
+    add.add_argument(
+        "prefix",
+        type=str,
+        help="The directory that the sub-repository will occupy in the combined repository"
+    )
+
+    add.add_argument(
+        "--branch", "-B",
+        type=str,
+        default="master",
+        help="Branch of the sub-repository to add to combined repository",
+    )
+
+    ##################################################
+    # init
+    init = subparsers.add_parser(
+        "init",
+        help="Initialize the combined repository"
+    )
+    init.set_defaults(func=cmd_init)
+    init.add_argument(
+        "repository",
+        type=str,
+        help="Relative path to the combined repository",
+    )
+
+    ##################################################
+    # push
+    push = subparsers.add_parser(
+        "push",
+        help="Push changes from the current repository to the combined repository"
+    )
+    push.set_defaults(func=cmd_push)
+
+    ##################################################
     # svn
     svn = subparsers.add_parser("svn").add_subparsers()
 
+    ##################################################
     # svn dcommit
     svn_dcommit = svn.add_parser("dcommit")
     svn_dcommit.set_defaults(func=cmd_svn_dcommit)
